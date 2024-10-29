a4d0de0452439b8d5606bcdc55fe89845b86ec32
aapetukhov <andreybsongs@gmail.com>
2024-10-29 21:31:18 +0300

    added model printing

--- train.py
+++ train.py
@@ -39,6 +39,7 @@ def main(config):
 
     # build model architecture, then print to console
     model = instantiate(config.model).to(device)
+    print(model)
     logger.info(model)
 
     # get function handles of loss and metrics
