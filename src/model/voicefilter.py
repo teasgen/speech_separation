9ee063bc3508b95b14b799396c1c97f8c2316a19
aapetukhov <andreybsongs@gmail.com>
2024-11-01 18:49:32 +0300

    add preliminary voicefilter version

--- src/model/voicefilter.py
+++ src/model/voicefilter.py
@@ -1,16 +1,108 @@
+import numpy as np
 import torch
+import torch.nn as nn
 
-class VoiceFilter:
-    # TODO: add model
-    def __init__(self, ...):
-        self.embedder = ...
+from lipreader.lipreading.utils import load_model, load_json
+from lipreader.lipreading.model import Lipreading
+from lipreader.lipreading.dataloaders import get_preprocessing_pipelines
 
-    def forward(self, mix_spectrogram, s1_video, s2_video):
-        s1_embedding = self.embedder(s1_video, mix_spectrogram)
-        s2_embedding = self.embedder(s2_video, mix_spectrogram)
 
-        s1_pred = s1_embedding * mix_spectrogram
-        s2_pred = s2_embedding * mix_spectrogram
+class VoiceFilter(nn.Module):
+    def __init__(
+            self,
+            lipreader_path: str = "lrw_snv1x_tcn1x.pth",
+            lipreader_config: str = "configs/lrw_snv1x_tcn1x.json",
+            max_audio_len: int = 32000
+    ):
+        super(VoiceFilter, self).__init__()
+
+        # initializing lipreader
+        args_loaded = load_json(lipreader_config)
+
+        tcn_options = {
+            'num_layers': args_loaded.get('tcn_num_layers', 4),
+            'kernel_size': args_loaded.get('tcn_kernel_size', [3]),
+            'dropout': args_loaded.get('tcn_dropout', 0.2),
+            'dwpw': args_loaded.get('tcn_dwpw', False),
+            'width_mult': args_loaded.get('tcn_width_mult', 1),
+        }
+
+        lipreader = Lipreading(
+            modality="video",
+            num_classes=500,
+            backbone_type=args_loaded['backbone_type'],
+            width_mult=args_loaded['width_mult'],
+            relu_type=args_loaded['relu_type'],
+            tcn_options=tcn_options,
+            use_boundary=args_loaded.get("use_boundary", False),
+            extract_feats=True
+        )
+
+        self.lipreader = load_model(
+            load_path=lipreader_path,
+            model=lipreader,
+            allow_size_mismatch=True
+        )
+        self.lipreader.eval()
+        self.preprocessing_func = get_preprocessing_pipelines(modality="video")["test"]
+
+        # gru layer for lip embeddings
+        self.gru = nn.GRU(input_size=1024, hidden_size=256, num_layers=2, bidirectional=True, batch_first=True)
+        
+        # fc to obtain dvector
+        self.fc_dvector = nn.Linear(256 * 2, 256)  # *2 bidirectional gru
+
+        # cnn for mix spec
+        self.cnn_layers = nn.Sequential(
+            nn.Conv2d(1, 64, (1, 7), dilation=(1, 1)), nn.ReLU(),
+            nn.Conv2d(64, 64, (7, 1), dilation=(1, 1)), nn.ReLU(),
+            nn.Conv2d(64, 64, (5, 5), dilation=(1, 1)), nn.ReLU(),
+            nn.Conv2d(64, 64, (5, 5), dilation=(2, 1)), nn.ReLU(),
+            nn.Conv2d(64, 64, (5, 5), dilation=(4, 1)), nn.ReLU(),
+            nn.Conv2d(64, 64, (5, 5), dilation=(8, 1)), nn.ReLU(),
+            nn.Conv2d(64, 64, (5, 5), dilation=(16, 1)), nn.ReLU(),
+            nn.Conv2d(64, 8, (1, 1), dilation=(1, 1)), nn.ReLU()
+        )
+
+        # lstm for mask
+        self.lstm = nn.LSTM(input_size=8 + 256, hidden_size=400, batch_first=True)
+
+        # fc for output
+        self.fc1 = nn.Linear(400, 600)
+        self.fc2 = nn.Linear(600, max_audio_len) 
+
+    def forward(self, mix_spectrogram, s1_video: np.ndarray, s2_video: np.ndarray):
+        s1_data = self.preprocessing_func(s1_video)
+        s2_data = self.preprocessing_func(s2_video)
+
+        s1_embedding = self.lipreader(torch.FloatTensor(s1_data)[None, None, :, :, :], lengths=[s1_data.shape[0]])  # 1 x T x C = (50, 1024)
+        s2_embedding = self.lipreader(torch.FloatTensor(s2_data)[None, None, :, :, :], lengths=[s2_data.shape[0]])  # 1 x T x C = (50, 1024)
+
+        # d-vectors
+        s1_gru, _ = self.gru(s1_embedding)
+        s2_gru, _ = self.gru(s2_embedding)
+        
+        s1_dvector = self.fc_dvector(s1_gru[:, -1, :])
+        s2_dvector = self.fc_dvector(s2_gru[:, -1, :])
+
+        mix_spectrogram = mix_spectrogram.unsqueeze(1)
+        cnn_out = self.cnn_layers(mix_spectrogram)
+
+        # concatenate dvectors to cnn out at each time frame
+        cnn_out = cnn_out.squeeze(2).permute(0, 2, 1)  # (B, T, 8)
+        s1_cnn_lstm_input = torch.cat((cnn_out, s1_dvector.expand_as(cnn_out)), dim=-1)
+        s2_cnn_lstm_input = torch.cat((cnn_out, s2_dvector.expand_as(cnn_out)), dim=-1)
+
+        # making masks
+        s1_lstm_out, _ = self.lstm(s1_cnn_lstm_input)
+        s2_lstm_out, _ = self.lstm(s2_cnn_lstm_input)
+
+        s1_fc_out = self.fc2(nn.ReLU(self.fc1(s1_lstm_out)))
+        s2_fc_out = self.fc2(nn.ReLU(self.fc1(s2_lstm_out)))
+
+        # apply masks
+        s1_pred = s1_fc_out * mix_spectrogram
+        s2_pred = s2_fc_out * mix_spectrogram
 
         return {"s1_pred": s1_pred, "s2_pred": s2_pred}
 
