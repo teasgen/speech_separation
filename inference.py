c17cfac6923668d649775fc66075d774441f62af
aapetukhov <andreybsongs@gmail.com>
2024-11-02 15:03:07 +0300

    change to cuda for kaggle

--- inference.py
+++ inference.py
@@ -27,8 +27,6 @@ def main(config):
     if config.inferencer.device == "auto":
         if torch.cuda.is_available():
             device = "cuda"
-        elif torch.backends.mps.is_available():
-            device = "mps"
         else:
             device = "cpu"
     else:
