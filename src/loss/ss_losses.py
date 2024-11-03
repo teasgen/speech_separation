6669ddbf3be25506c9e1bfb041a036556c91b9eb
teasgen <vladms03@yandex.ru>
2024-11-03 17:29:39 +0300

    add SiSNRLoss

--- src/loss/ss_losses.py
+++ src/loss/ss_losses.py
@@ -75,3 +75,35 @@ class MAEWavLoss(BaseSSLoss):
 
 
 # TODO: add custom loss for VoiceFilter (?)
+    def forward(self, s1_pred: torch.Tensor, s2_pred: torch.Tensor, s1: torch.Tensor, s2: torch.Tensor, **batch):
+        return super().forward(s1_pred, s2_pred, s1, s2)
+
+
+class SiSNRLoss(nn.Module):
+    def __init__(self):
+        super(SiSNRLoss, self).__init__()
+
+    def forward(self, pred: torch.Tensor, gt: torch.Tensor, **batch):
+        pred = pred - torch.mean(pred, dim=-1, keepdim=True)
+        gt = gt - torch.mean(gt, dim=-1, keepdim=True)
+
+        dot_product = (gt * pred).sum(dim=-1, keepdim=True)
+        gt_energy = torch.linalg.norm(gt, ord=2, dim=-1, keepdim=True) ** 2
+        scaling_factor = dot_product / gt_energy
+
+        scaled_gt = scaling_factor * gt
+
+        e_noise = pred - scaled_gt
+        signal_power = torch.linalg.norm(scaled_gt, ord=2, dim=-1, keepdim=True) ** 2
+        noise_power = torch.linalg.norm(e_noise, ord=2, dim=-1, keepdim=True) ** 2
+
+        return (-20 * torch.log10(signal_power / noise_power)).mean()
+
+
+class SiSNRWavLoss(BaseSSLoss):
+    def __init__(self):
+        loss = SiSNRLoss()
+        super().__init__(loss)
+
+    def forward(self, s1_pred: torch.Tensor, s2_pred: torch.Tensor, s1: torch.Tensor, s2: torch.Tensor, **batch):
+        return super().forward(s1_pred, s2_pred, s1, s2)
