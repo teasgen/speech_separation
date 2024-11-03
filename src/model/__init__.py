b41776febedec35594452a5f71d9a4b582ef78ea
Vladislav Smirnov <teasgen@qavm-5b9e12ac.qemu>
2024-11-03 19:12:19 +0300

    init dprnn

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,6 @@
 from src.model.baseline_model import BaselineModel
 from src.model.baseline_ss_model import SSBaselineModel
 from src.model.voicefilter import VoiceFilter
+from src.model.dprnn import DPRNNEncDec
 
 __all__ = ["BaselineModel", "SSBaselineModel", "VoiceFilter"]
