f1d89c185b7ccd5a1982d227a1c1d6626fd6546c
Вера <vera@Vera--Notebook.local>
2024-11-06 20:52:08 +0300

    add configs

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,7 +1,9 @@
 from src.model.baseline_model import BaselineModel
 from src.model.baseline_ss_model import SSBaselineModel
+from src.model.convtasnet import ConvTasNet
 
 __all__ = [
     "BaselineModel",
     "SSBaselineModel",
+    "ConvTasNet",
 ]
