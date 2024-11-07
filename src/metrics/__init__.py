a08e657d93ca772c7af6a4317e0be8d0ea25a2ca
aapetukhov <andreybsongs@gmail.com>
2024-11-07 17:41:55 +0300

    fix import bug

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -1,6 +1,7 @@
 from src.metrics.pesq import PESQMetric
 from src.metrics.si_sdr import SISDRMetric
-from src.metrics.si_snr import SISNRiMetric, SISNRMetric
+from src.metrics.si_snr import SISNRMetric
+from src.metrics.si_snri import SISNRiMetric
 from src.metrics.stoi import STOIMetric
 
 __all__ = ["PESQMetric", "SISDRMetric", "SISNRMetric", "STOIMetric", "SISNRiMetric"]
