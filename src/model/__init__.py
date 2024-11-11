0e60c36ae787f89e3bb2c10a4f927d39dd29e535
Вера <vera@Vera--Notebook.local>
2024-11-11 18:43:16 +0300

    add av_convtasnet

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,5 @@
 from src.model.baseline_model import BaselineModel
 from src.model.baseline_ss_model import SSBaselineModel
-from src.model.voicefilter import VoiceFilter
+from src.model.dual_rnn import Dual_RNN_model
 
-__all__ = ["BaselineModel", "SSBaselineModel", "VoiceFilter"]
+__all__ = ["BaselineModel", "SSBaselineModel", "Dual_RNN_model"]
